45e1ac895cca11601794e40d1fa1899167165225
Douglas Vidal <vidaldl01@gmail.com>
2024-03-06 14:17:49 -0300

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${poppins.className} overflow-x-hidden scroll-smooth`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Separator } from "@/components/ui/separator";
+
+function Footer() {
+  return (
+    <footer className="w-full my-8 min-h-fit text-secondary/70">
+      <div className="flex flex-wrap md:items-center items-start justify-between container text-base">
+        <div>
+          <Link
+            href="#inicio"
+            className="font-semibold tracking-wider transition duration-300
+              hover:text-secondary"
+          >
+            <h1>@douglvv</h1>
+          </Link>
+        </div>
+
+        <div>
+          <ul className="list-none flex flex-col md:flex-row flex-wrap gap-2">
+            <li className="list-item">
+              <Button variant={"link"} asChild className="text-secondary/70">
+                <Link
+                  href="#inicio"
+                  className="font-semibold tracking-wider 
+                    transition duration-300"
+                >
+                  Início
+                </Link>
+              </Button>
+            </li>
+            <li className="list-item">
+              <Button variant={"link"} asChild className="text-secondary/70">
+                <Link
+                  href="#sobre"
+                  className="font-semibold tracking-wider 
+                    transition duration-300"
+                >
+                  Sobre
+                </Link>
+              </Button>
+            </li>
+            <li className="list-item">
+              <Button variant={"link"} asChild className="text-secondary/70">
+                <Link
+                  href="#projetos"
+                  className="font-semibold tracking-wider 
+                    transition duration-300"
+                >
+                  Projetos
+                </Link>
+              </Button>
+            </li>
+            <li className="list-item">
+              <Button variant={"link"} asChild className="text-secondary/70">
+                <Link
+                  href="#contato"
+                  className="font-semibold tracking-wider"
+                >
+                  Contato
+                </Link>
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="container">
+        <Separator className="my-8 bg-secondary/70" />
+      </div>
+
+      <div className="flex items-start justify-start container text-secondary/50">
+        <p>© 2024 by @douglvv</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
